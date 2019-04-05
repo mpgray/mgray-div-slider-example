904344e5e84550d2d9f004b05e9028b73056a1b3
Michael Gray <michael@mgray.me>
2019-04-04 23:58:35 -0600

    created observables

--- src/app/div-example/div-example.component.ts
+++ src/app/div-example/div-example.component.ts
@@ -1,5 +1,7 @@
 import {Component, NgZone, OnInit} from '@angular/core';
-import {MatSliderModule} from '@angular/material/slider';
+import {MatSliderModule} from '@angular/material';
+import {RESTService} from './div-example.service';
+import update from '@angular/cli/commands/update';
 
 @Component({
   selector: 'app-div-example',
@@ -8,12 +10,44 @@ import {MatSliderModule} from '@angular/material/slider';
 })
 export class DivExampleComponent implements OnInit {
   sliderNum = 500;
-  constructor() {
+  people: any = [];
+  temp: any = [];
+  constructor(public rest: RESTService) {
 
   }
 
   ngOnInit() {
-
+    this.getPeople();
   }
+
+  getPeople() {
+    this.people = [];
+    this.rest.getExampleItem().subscribe((data: {}) => {
+      console.log(data);
+      this.people = data;
+      console.log(this.people.quantity);
+      this.updateSlider(this.people.quantity);
+    });
+    this.updateSlider(this.people.quantity);
+  }
+
+  updatePeople() {
+    this.people = [];
+    this.rest.updateExampleItem(this.people).subscribe((data: {}) => {
+      console.log(data);
+      this.people = data;
+      console.log(this.people.quantity);
+      this.updateSlider(this.people.quantity);
+    });
+    this.updateSlider(this.people.quantity);
+  }
+
+
+
+  updateSlider(s) {
+    this.sliderNum = s;
+  }
+
+
 
 }
--- src/app/div-example/div-example.service.ts
+++ src/app/div-example/div-example.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
@@ -17,11 +17,21 @@ export class RESTService {
   constructor(private http: HttpClient) {
   }
 
+  private extractData(res: Response) {
+    const body = res;
+    return body || {};
+  }
+
   updateExampleItem(product): Observable<any> {
     return this.http.put(endpoint + 'example/item' + '/' + 90, JSON.stringify(product), httpOptions).pipe(
       tap(_ => console.log(`updated product id=${90}`)),
       catchError(this.handleError<any>('updateItem'))
     );
+  }
+
+  getExampleItem(): Observable<any> {
+    return this.http.get(endpoint + 'example/item' + '/' + 90).pipe(
+      map(this.extractData));
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
